1c74f7a081841413f06b83b36d16be4618b08881
KLINEKRAFT <colin.ikline@gmail.com>
2026-09-04 13:49:45 -0500

    Replace flat ground with generated valley environment

--- unreal/Source/FirstVillage/CivVillageGameMode.cpp
+++ unreal/Source/FirstVillage/CivVillageGameMode.cpp
@@ -1,7 +1,6 @@
 #include "CivVillageGameMode.h"
 #include "CivWorldDirector.h"
-#include "Engine/StaticMeshActor.h"
-#include "Components/StaticMeshComponent.h"
+#include "CivRuntimeEnvironment.h"
 #include "GameFramework/DefaultPawn.h"
 #include "NavMesh/NavMeshBoundsVolume.h"
 #include "NavigationSystem.h"
@@ -21,18 +20,9 @@ void ACivVillageGameMode::BeginPlay()
 
 void ACivVillageGameMode::SpawnRuntimeGround()
 {
-    UWorld* World = GetWorld();
-    if (!World) return;
-
-    AStaticMeshActor* Ground = World->SpawnActor<AStaticMeshActor>(FVector(0.f, 0.f, -55.f), FRotator::ZeroRotator);
-    if (!Ground) return;
-
-    UStaticMesh* Cube = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Cube.Cube"));
-    if (Cube)
+    if (UWorld* World = GetWorld())
     {
-        Ground->GetStaticMeshComponent()->SetStaticMesh(Cube);
-        Ground->GetStaticMeshComponent()->SetWorldScale3D(FVector(65.f, 65.f, 0.5f));
-        Ground->GetStaticMeshComponent()->SetCollisionProfileName(TEXT("BlockAll"));
+        World->SpawnActor<ACivRuntimeEnvironment>(FVector::ZeroVector, FRotator::ZeroRotator);
     }
 }
 
@@ -41,10 +31,10 @@ void ACivVillageGameMode::SpawnNavigationBounds()
     UWorld* World = GetWorld();
     if (!World) return;
 
-    ANavMeshBoundsVolume* NavBounds = World->SpawnActor<ANavMeshBoundsVolume>(FVector(0.f, 0.f, 400.f), FRotator::ZeroRotator);
+    ANavMeshBoundsVolume* NavBounds = World->SpawnActor<ANavMeshBoundsVolume>(FVector(0.f, 0.f, 700.f), FRotator::ZeroRotator);
     if (!NavBounds) return;
 
-    NavBounds->SetActorScale3D(FVector(32.f, 32.f, 8.f));
+    NavBounds->SetActorScale3D(FVector(42.f, 42.f, 14.f));
     if (UNavigationSystemV1* Nav = FNavigationSystem::GetCurrent<UNavigationSystemV1>(World))
     {
         Nav->OnNavigationBoundsUpdated(NavBounds);
