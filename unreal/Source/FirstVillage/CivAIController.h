c13cd62c7dfc41c1e9e61c915d65330c6794969b
KLINEKRAFT <colin.ikline@gmail.com>
2026-09-04 13:59:17 -0500

    Add Astra decision execution to Unreal AI controllers

--- unreal/Source/FirstVillage/CivAIController.h
+++ unreal/Source/FirstVillage/CivAIController.h
@@ -4,6 +4,9 @@
 #include "AIController.h"
 #include "CivAIController.generated.h"
 
+class ACivResourceNode;
+class ACivAgentCharacter;
+
 UCLASS()
 class FIRSTVILLAGE_API ACivAIController : public AAIController
 {
@@ -12,11 +15,39 @@ class FIRSTVILLAGE_API ACivAIController : public AAIController
 public:
     ACivAIController();
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Agent Brain")
+    bool bUseRemoteAgent = true;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Agent Brain")
+    float DecisionIntervalMin = 12.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Agent Brain")
+    float DecisionIntervalMax = 24.f;
+
 protected:
     virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
     virtual void Tick(float DeltaSeconds) override;
+    virtual void OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result) override;
 
 private:
     float DecisionTimer = 0.f;
+    bool bDecisionInFlight = false;
+    FName ActiveAction = NAME_None;
+
+    UPROPERTY()
+    TObjectPtr<ACivResourceNode> ActiveResource;
+
+    UPROPERTY()
+    TObjectPtr<ACivAgentCharacter> ActiveOtherAgent;
+
     void ChooseLocalMovementGoal();
+    void RequestRemoteDecision();
+    FString BuildObservationJson() const;
+    void ExecuteDecisionJson(const FString& ResponseJson);
+    void ExecuteBuildDecision(const TSharedPtr<class FJsonObject>& DecisionObject);
+    ACivResourceNode* FindNearestResource(FName ResourceName) const;
+
+    UFUNCTION()
+    void HandleRemoteDecision(int32 AgentId, bool bSuccess, const FString& ResponseJson);
 };
