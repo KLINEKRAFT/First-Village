06dd6f3e1ecc791b9b3cb473aae6c8223cb20c84
KLINEKRAFT <colin.ikline@gmail.com>
2026-09-04 13:54:37 -0500

    Use canonical validated building blueprint in 3D builder

--- unreal/Source/FirstVillage/CivProceduralBuilding.h
+++ unreal/Source/FirstVillage/CivProceduralBuilding.h
@@ -2,34 +2,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "CivBuildingValidator.h"
 #include "CivProceduralBuilding.generated.h"
-
-USTRUCT(BlueprintType)
-struct FCivBuildingBlueprint
-{
-    GENERATED_BODY()
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    FString Name = TEXT("Hut");
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    FString Purpose = TEXT("Housing");
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    FVector2D Footprint = FVector2D(500.f, 400.f);
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float WallHeight = 280.f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    bool bHasRoof = true;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    bool bHasHearth = false;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    int32 PostCount = 6;
-};
 
 UCLASS()
 class FIRSTVILLAGE_API ACivProceduralBuilding : public AActor
@@ -57,4 +31,5 @@ private:
 
     UStaticMeshComponent* AddPrimitive(const FName& Name, const FVector& RelativeLocation, const FVector& Scale, const FRotator& Rotation = FRotator::ZeroRotator);
     void ClearGeneratedParts();
+    bool HasPrimitive(FName Primitive) const;
 };
