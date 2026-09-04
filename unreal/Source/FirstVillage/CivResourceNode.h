95ec7ab9ce8e264718bfb1db63c0d472183bc2c2
KLINEKRAFT <colin.ikline@gmail.com>
2026-09-04 13:43:32 -0500

    Add 3D resource node actor

---
+++ unreal/Source/FirstVillage/CivResourceNode.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CivResourceNode.generated.h"
+
+UENUM(BlueprintType)
+enum class ECivResourceType : uint8
+{
+    Water,
+    Food,
+    Wood,
+    Stone,
+    Clay,
+    Game
+};
+
+UCLASS()
+class FIRSTVILLAGE_API ACivResourceNode : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ACivResourceNode();
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Resource")
+    ECivResourceType ResourceType = ECivResourceType::Wood;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Resource")
+    float Quantity = 100.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Resource")
+    bool bRenewable = false;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Resource")
+    TObjectPtr<UStaticMeshComponent> Mesh;
+
+    UFUNCTION(BlueprintCallable, Category="Resource")
+    float Harvest(float RequestedAmount);
+};
