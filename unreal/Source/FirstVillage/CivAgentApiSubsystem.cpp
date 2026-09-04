7da90c402e16c5dcf5e47b83be069e0df5d41d84
KLINEKRAFT <colin.ikline@gmail.com>
2026-09-04 13:47:08 -0500

    Implement Unreal HTTP requests to First Village agent API

---
+++ unreal/Source/FirstVillage/CivAgentApiSubsystem.cpp
@@ -0,0 +1,21 @@
+#include "CivAgentApiSubsystem.h"
+#include "HttpModule.h"
+#include "Interfaces/IHttpRequest.h"
+#include "Interfaces/IHttpResponse.h"
+
+void UCivAgentApiSubsystem::RequestDecision(const FString& ObservationJson)
+{
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
+    Request->SetURL(DecisionEndpoint);
+    Request->SetVerb(TEXT("POST"));
+    Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+    Request->SetContentAsString(ObservationJson);
+
+    Request->OnProcessRequestComplete().BindLambda([this](FHttpRequestPtr Req, FHttpResponsePtr Resp, bool bConnectedSuccessfully)
+    {
+        const bool bOk = bConnectedSuccessfully && Resp.IsValid() && EHttpResponseCodes::IsOk(Resp->GetResponseCode());
+        OnDecisionReceived.Broadcast(bOk, Resp.IsValid() ? Resp->GetContentAsString() : FString());
+    });
+
+    Request->ProcessRequest();
+}
