4072558a0eb8fa412dbbb55f1cda91788e536d4d
KLINEKRAFT <colin.ikline@gmail.com>
2026-09-04 13:24:11 -0500

    Add Unreal module build rules

---
+++ unreal/Source/FirstVillage/FirstVillage.Build.cs
@@ -0,0 +1,19 @@
+using UnrealBuildTool;
+
+public class FirstVillage : ModuleRules
+{
+    public FirstVillage(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "Json",
+            "JsonUtilities",
+            "AIModule",
+            "NavigationSystem"
+        });
+    }
+}
