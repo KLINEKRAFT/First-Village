efb9e0e9e23febe1efb17bbc2ea7d538576c448e
KLINEKRAFT <colin.ikline@gmail.com>
2026-09-04 13:45:28 -0500

    Render placeholder humanoids in UE5 simulation

--- unreal/Source/FirstVillage/CivAgentCharacter.cpp
+++ unreal/Source/FirstVillage/CivAgentCharacter.cpp
@@ -1,11 +1,22 @@
 #include "CivAgentCharacter.h"
 #include "CivAgentMindComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
 ACivAgentCharacter::ACivAgentCharacter()
 {
     PrimaryActorTick.bCanEverTick = true;
     Mind = CreateDefaultSubobject<UCivAgentMindComponent>(TEXT("Mind"));
+
+    BodyVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyVisual"));
+    BodyVisual->SetupAttachment(GetRootComponent());
+    BodyVisual->SetRelativeLocation(FVector(0.f, 0.f, -8.f));
+    BodyVisual->SetRelativeScale3D(FVector(0.34f, 0.34f, 0.9f));
+    BodyVisual->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    if (UStaticMesh* BodyMesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Capsule.Capsule")))
+    {
+        BodyVisual->SetStaticMesh(BodyMesh);
+    }
 
     GetCharacterMovement()->MaxWalkSpeed = 260.f;
     GetCharacterMovement()->bOrientRotationToMovement = true;
